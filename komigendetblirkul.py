f243e564fee4fd787ccb4725089dc009e88305de
Rupus Reinefjord <rupus@kltrst.se>
2016-11-22 16:12:15 +0100

    less code is good code.
    always. no exceptions.

--- komigendetblirkul.py
+++ komigendetblirkul.py
@@ -4,16 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def random_phrase():
-    with open("komigendetblirkul.db") as f:
-        db = [line for line in (line.strip() for line in f) if line]
-
-    phrase = random.choice(db)
-
-    return render_template("komigendetblirkul.html", phrase=phrase)
-
-
+@app.route("/", defaults={"phrase": ""})
 @app.route("/<path:phrase>")
-def user_phrase(phrase):
+def index(phrase):
+    if not phrase:
+        with open("komigendetblirkul.db") as f:
+            db = [line for line in (line.strip() for line in f) if line]
+        phrase = random.choice(db)
+
     return render_template("komigendetblirkul.html", phrase=phrase)
