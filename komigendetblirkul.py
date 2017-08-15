7fbbf401506c9406cf2104b446e4a0f783866267
Rupus Reinefjord <rupus@kltrst.se>
2017-08-15 13:57:14 +0200

    add optional base64 encoding

--- komigendetblirkul.py
+++ komigendetblirkul.py
@@ -1,10 +1,19 @@
+import base64
 import random
-from flask import Flask, render_template
+from flask import abort, Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/e/<b64>")
+def encoded(b64):
+    try:
+        phrase = base64.urlsafe_b64decode(b64).decode()
+    except:
+        return index(request.full_path)
 
-@app.route("/", defaults={"phrase": ""})
+    return index(phrase)
+
+@app.route("/", defaults={"phrase": None})
 @app.route("/<path:phrase>")
 def index(phrase):
     if not phrase:
@@ -12,4 +21,6 @@ def index(phrase):
             db = [line for line in (line.strip() for line in f) if line]
         phrase = random.choice(db)
 
-    return render_template("komigendetblirkul.html", phrase=phrase)
+    b64 = base64.urlsafe_b64encode(phrase.encode()).decode()
+
+    return render_template("komigendetblirkul.html", phrase=phrase, b64=b64)
