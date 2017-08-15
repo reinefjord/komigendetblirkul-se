1c1f2bf701492b2951de539dbd0cc305b0f87b56
Rupus Reinefjord <rupus@kltrst.se>
2017-08-15 14:30:56 +0200

    .index for b64, check for b64 everytime

--- komigendetblirkul.py
+++ komigendetblirkul.py
@@ -4,19 +4,16 @@ from flask import abort, Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/e/<b64>")
-def encoded(b64):
-    try:
-        phrase = base64.urlsafe_b64decode(b64).decode()
-    except:
-        return index(request.path[1:])
-
-    return index(phrase)
 
 @app.route("/", defaults={"phrase": None})
 @app.route("/<path:phrase>")
 def index(phrase):
-    if not phrase:
+    if phrase:
+        try:
+            phrase = base64.urlsafe_b64decode(phrase).decode()
+        except:
+            pass
+    else:
         with open("komigendetblirkul.db") as f:
             db = [line for line in (line.strip() for line in f) if line]
         phrase = random.choice(db)
