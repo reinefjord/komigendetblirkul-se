d6dbb13b384c8532288a4aa3171a1350d5b87f5d
Martin Alge <martin@alge.se>
2017-11-22 13:43:55 +0100

    fixed so a favicon is locaded at /favicon.ico

--- komigendetblirkul.py
+++ komigendetblirkul.py
@@ -1,6 +1,6 @@
 import base64
 import random
-from flask import abort, Flask, render_template, request
+from flask import abort, Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route("/", defaults={"phrase": None})
 @app.route("/<path:phrase>")
 def index(phrase):
+
+    if request.path == "/favicon.ico":
+        return send_file("favicon.ico")
+
     if phrase:
         try:
             phrase = base64.urlsafe_b64decode(phrase).decode()
@@ -17,6 +21,7 @@ def index(phrase):
         with open("komigendetblirkul.db") as f:
             db = [line for line in (line.strip() for line in f) if line]
         phrase = random.choice(db)
+
 
     b64 = base64.urlsafe_b64encode(phrase.encode()).decode()
 
